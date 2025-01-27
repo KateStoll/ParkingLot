9fe076b84abda094bb1ee39f8710150348823451
jtzero ? <jtzero511@gmail.com>
2025-01-26 23:16:52 -0500

    CRUD for parking-spaces works

--- backend/parking_lot/README.md
+++ backend/parking_lot/README.md
@@ -1,11 +1,41 @@
 
 # Setup
-`poetry run python manage.py createsuperuser`
-`poetry run python manage.py migrate`
+`asdf install`
+`poetry install`
+`poetry poe manage createsuperuser` # username password doesn't matter
+`poetry poe manage migrate`
 
 # Run
-`poetry run python manage.py runserver`
+`poetry poe manage.py runserver`
+
+# curl example
+```bash
+curl -X POST -d '{"username": "$user_name_from_createsuperuser","password": "$password_from_createsuperuser"}' \
+     -H 'Content-Type: application/json'  http://127.0.0.1:8000/api/auth/token/login/
+```
+response =>
+`{"auth_token":"63eff0b0bebeae009766a55d0f069b058b01fa5d"}`
+
+```bash
+curl -X POST -H 'Authorization: Token $token_from_previous_example' \
+     -H 'Accept: application/json; indent=4'
+     -H 'Content-Type: application/json'
+     -d '{"taken": true}'  http://127.0.0.1:8000/api/v1/parking-spaces
+```
+response =>
+```json
+{
+    "id": 12,
+    "taken": true,
+    "created": "2025-01-28T01:43:23.829940Z",
+    "updated": "2025-01-28T01:43:23.829984Z"
+}
+```
+
+
+
+# See routes
+`poetry poe manage show_urls`
 
 # Test
-`poetry self add poethepoet`
 `poetry poe test`
--- backend/parking_lot/parking_spaces/views.py
+++ backend/parking_lot/parking_spaces/views.py
@@ -1,63 +1,15 @@
 from django.shortcuts import render
-from rest_framework import generics
+from django.views.decorators.http import require_http_methods
+from django.contrib.auth import login, logout
+from rest_framework import generics, permissions, viewsets, status, views
+from rest_framework.response import Response
+from rest_framework.decorators import action
+from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 from parking_spaces import serializers
 from parking_spaces.models import ParkingSpace
 from parking_spaces.serializers import ParkingSpaceSerializer
-from rest_framework import permissions, viewsets
-from rest_framework.permissions import IsAuthenticated, IsAuthenticatedOrReadOnly
 
 class ParkingSpaceView(viewsets.ModelViewSet):
     queryset = ParkingSpace.objects.all()
     serializer_class = ParkingSpaceSerializer 
     permission_classes=[IsAuthenticated,]
-    
-    def get_permissions(self):
-        if self.request.method == 'GET':
-            return [IsAuthenticatedOrReadOnly()]
-        return super().get_permissions()
-
-    
-    def post(self, request, id, *args, **kwargs):
-        try:
-            
-            parking_spaces = ParkingSpace.objects.get(id=id)
-            parking_space_data = request.data.get('parking_space')
-            
-            serializer_context = self.get_serializer_context()
-            serializer_context['parking_space'] = parking_space
-            
-            serializer = self.get_serializer(data=parking_space_data, context=serializer_context)
-            serializer.is_valid(raise_exception=True)
-            self.perform_create(serializer)
-        
-            return Response({ "parking_space": serializer.data } ,status=status.HTTP_200_OK)
-            
-        except Exception:
-            return Response({"errors": {
-                "body": [
-                    "Bad Request"
-                ]
-            }}, status=status.HTTP_404_NOT_FOUND)
-            
-        
-    
-    def list(self, request, id, *args, **kwargs):
-        try:
-        
-            parking_spaces = ParkingSpace.objects.get(id=id)
-            
-            serializer = self.get_serializer(parking_spaces, many=True)
-            
-            response = {
-                'parking_spaces' : serializer.data, 
-            }
-            
-            return Response(response)
-            
-        except Exception:
-            return Response({"errors": {
-                "body": [
-                    "Bad Request"
-                ]
-            }}, status=status.HTTP_404_NOT_FOUND)
-
